5ce1e74b92d6faac64d4552a552399877d983edf
weixuansun <weixuansun7@gmail.com>
2023-03-28 12:52:33 +0800

    Update test_vggss.sh

--- test_vggss.sh
+++ test_vggss.sh
@@ -6,11 +6,6 @@ python test.py
     --testset 'vggss' \
     --alpha 0.4 \
     --gpu 0 \
-    --model asy \
-    --use_momentum \
-    --use_mom_eval \
-    --m_img 0.999 \
-    --m_aud 0.999 \
     --dropout_img 0.9 \
     --dropout_aud 0 \
     # --save_visualizations \
