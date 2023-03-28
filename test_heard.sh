876550704d66a13ecbc032e05c69cdea70056cbf
weixuansun <weixuansun7@gmail.com>
2023-03-28 12:51:37 +0800

    Update test_heard.sh

--- test_heard.sh
+++ test_heard.sh
@@ -7,11 +7,7 @@ python test.py
     --testset 'vggss_heard' \
     --alpha 0.4 \
     --gpu 0 \
-    --model ez_vsl \
-    --use_momentum \
     --use_mom_eval \
-    --m_img 0.999 \
-    --m_aud 0.999 \
     --dropout_img 0.9 \
     --dropout_aud 0 \
     # --save_visualizations \
@@ -26,11 +22,7 @@ python test.py
     --testset 'vggss_unheard' \
     --alpha 0.4 \
     --gpu 0 \
-    --model ez_vsl \
-    --use_momentum \
     --use_mom_eval \
-    --m_img 0.999 \
-    --m_aud 0.999 \
     --dropout_img 0.9 \
     --dropout_aud 0 \
     # --save_visualizations \
